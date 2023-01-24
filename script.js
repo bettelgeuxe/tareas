306f78220dbb742a64dc92df434dbaaf5ddc10be
bettelgeuxe <68313088+bettelgeuxe@users.noreply.github.com>
2023-01-24 11:41:46 -0500

    cambia contenido primera tarea

--- script.js
+++ script.js
@@ -5,8 +5,21 @@ console.log(btn);
 const creatTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
-}
+    const value = input.value;
+    const task = document.querySelector("[data-task]");
+    input.value = '';
+    
+
+    const content = `<div>
+              <i class="far fa-check-square icon"></i>
+              <span class="task">${value}</span>
+            </div>
+            <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+            task.innerHTML = content;
+
+            console.log(content);
+};
 //arrow function - funciones anónimas
 //btn.addEventListener("click", function(evento){
 /*btn.addEventListener("click", (evento) => {
