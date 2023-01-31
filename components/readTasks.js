0ae73634ed3a265514b8159fb787c7b4fb03a99b
bettelgeuxe <68313088+bettelgeuxe@users.noreply.github.com>
2023-01-31 18:16:22 -0500

    agregar tareas y agrupar por fecha

--- components/readTasks.js
+++ components/readTasks.js
@@ -5,16 +5,20 @@ import dateElement from './dateElement.js'
 export const readTask = () =>{
 
     const list = document.querySelector('[data-list]');
-    console.log(list);
-
     const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
     const dates = uniqueDates(taskList);
 
     dates.forEach( date => {
-        console.log(date);
+        
+        const dateMoment = moment(date, "DD/MM/YYYY");
+        
         list.appendChild(dateElement(date));
         taskList.forEach((task) => {
-            list.appendChild(creatTask(task));
+            const taskDate = moment(task.dateFormat, "DD/MM/YYYY");
+            const diff = dateMoment.diff(taskDate);
+            if (diff === 0){
+                list.appendChild(creatTask(task));
+            }
         });
     });
 };
