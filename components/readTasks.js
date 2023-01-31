cd8b5a369a20a095c27b2c9a58f8a495c9a9b7e2
bettelgeuxe <68313088+bettelgeuxe@users.noreply.github.com>
2023-01-31 18:08:10 -0500

    tareas con fecha superior pendiente agrupar

--- components/readTasks.js
+++ components/readTasks.js
@@ -1,12 +1,20 @@
 import { creatTask } from "./addTask.js";
+import { uniqueDates } from "../services/date.js";
+import dateElement from './dateElement.js'
 
 export const readTask = () =>{
 
-    const list = document.querySelector("[data-list]");
+    const list = document.querySelector('[data-list]');
     console.log(list);
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-    
-    taskList.forEach((task) =>{
-        list.appendChild(creatTask(task));
-    })
+
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    const dates = uniqueDates(taskList);
+
+    dates.forEach( date => {
+        console.log(date);
+        list.appendChild(dateElement(date));
+        taskList.forEach((task) => {
+            list.appendChild(creatTask(task));
+        });
+    });
 };
